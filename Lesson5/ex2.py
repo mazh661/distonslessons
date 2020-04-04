6183fd1f0ba3b8139f378185c6a6757a97a31928
mazh661 <56090629+mazh661@users.noreply.github.com>
2020-04-04 19:58:35 +0600

    Lesson5p

---
+++ Lesson5/ex2.py
@@ -0,0 +1,12 @@
+arr=[42,123,3894,212,422]
+
+arr1=arr[0:3]
+arr2=arr[3:5]
+
+print(arr[0:4])
+
+print(arr[1:5])
+
+print(arr[1:]);
+
+print(arr[1:5:2])
---
+++ Lesson5/ex4.py
@@ -0,0 +1,8 @@
+arr=[1,"asdasd",1,"asdadsa",300,"asdasda",2000,"!!",300,"asdsad",300,"asdasd"]
+words=[]
+numbers=[]
+numbers=[arr[::2]]
+words=[arr[1::2]]
+
+print(numbers)
+print(words)
